6db710fc5f9b586c3e45f7982bdc5de9019edb0e
maximopasny <maximdudin1@gmail.com>
2016-10-17 11:32:18 +0300

    one more

--- topology_generator.py
+++ topology_generator.py
@@ -7,10 +7,12 @@ INNER_RANGE = 1
 OUTER_RANGE = 2
 NUM_OF_BS = 2
 
+
 class Statuses:
     FREE = 0
     CORRUPTED = 1
     STATION = 2
+
 
 class Point:
     def __init__(self, x, y):
@@ -33,40 +35,39 @@ def points_distance(point1, point2):
     return sqrt(pow(2, (point1.x - point2.x)) + pow(2, (point1.y - point2.y)))
 
 
-def linear_to_bin(lin_position):
-    return lin_position % X_SIZE, lin_position // X_SIZE
-
-
-def bin_to_linear(x, y):
-    return y * X_SIZE + x
-
-
 def generate_topology():
-
-    chosen_point = choice(points)
-    points[bin_to_linear(chosen_point.x, chosen_point.y)].status = Statuses.STATION
     available_points = []
-    for point in points:
-        if points_distance(point, chosen_point) <= INNER_RANGE and not chosen_point.equals(point) and not point.status == Statuses.STATION:
-            points[bin_to_linear(point.x, point.y)].status = Statuses.CORRUPTED
-        if INNER_RANGE < points_distance(point, chosen_point) <= OUTER_RANGE and not point.status == Statuses.STATION:
-            available_points.append(point)
-
-    for i in range(0, NUM_OF_BS - 1):
-        chosen_point = choice[available_points]
-        points[bin_to_linear(chosen_point.x, chosen_point.y)].status = Statuses.STATION
-        for point in points:
-            if points_distance(point, chosen_point) <= INNER_RANGE and not chosen_point.equals(point) and not point.status == Statuses.STATION:
-                points[bin_to_linear(point.x, point.y)].status = Statuses.CORRUPTED
+    chosen_point = choice(choice(points))
+    points[chosen_point.x][chosen_point.y].status = Statuses.STATION
+    for x_row in points:
+        for point in x_row:
+            if points_distance(point, chosen_point) <= INNER_RANGE and not chosen_point.equals(point) and point.status != Statuses.STATION:
+                points[point.x][point.y].status = Statuses.CORRUPTED
             if INNER_RANGE < points_distance(point, chosen_point) <= OUTER_RANGE and not point.status == Statuses.STATION:
                 available_points.append(point)
 
-        for point in available_points:
-            if points[bin_to_linear(point.x, point.y)].status == Statuses.CORRUPTED or points[bin_to_linear(point.x, point.y)].status == Statuses.STATION:
-                available_points.remove(point)
+    for i in range(0, NUM_OF_BS - 1):
+        iteration_step(available_points)
 
     for point in points:
         if point.status == Statuses.STATION:
             print(point.x, point.y)
+
+
+def iteration_step(available_points):
+    chosen_point = choice[available_points]
+    points[chosen_point.x][chosen_point.y].status = Statuses.STATION
+    for x_row in points:
+        for point in x_row:
+            if points_distance(point, chosen_point) <= INNER_RANGE and not chosen_point.equals(point) and point.status != Statuses.STATION:
+                points[point.x][point.y].status = Statuses.CORRUPTED
+            if INNER_RANGE < points_distance(point, chosen_point) <= OUTER_RANGE and not point.status == Statuses.STATION:
+                available_points.append(point)
+
+    for point in available_points:
+        if points[point.x][point.y].status == Statuses.CORRUPTED or points[point.x][point.y].status == Statuses.STATION:
+            available_points.remove(point)
+
+
 
 generate_topology()
