5a84bf4cb7c0838295c5cae71e8fd3f91132a0d9
maximopasny <maximdudin1@gmail.com>
2016-11-09 12:30:22 +0300

    one more

--- topology_generator.py
+++ topology_generator.py
@@ -19,10 +19,6 @@ def generate_topology():
     for i in range(0, NUM_OF_BS - 1):
         iteration_step(available_points)
 
-    for x_row in points:
-        for point in x_row:
-            if point.status == Status.STATION:
-                print(point.x, point.y)
 
 
 def iteration_step(available_points):
@@ -32,7 +28,7 @@ def iteration_step(available_points):
 
 
 def get_available_points():
-    available_points =[]
+    available_points = []
 
     for x_row in points:
         for point in x_row:
@@ -43,14 +39,33 @@ def get_available_points():
 
 
 def go_iter(chosen_point):
+    chosen_point.status = Status.STATION
     points[chosen_point.x][chosen_point.y].status = Status.STATION
+    #print("station ", chosen_point.x, chosen_point.y)
     for x_row in points:
         for point in x_row:
             if points_distance(point, chosen_point) <= INNER_RANGE and not chosen_point.equals(point) and not point.status == Status.STATION:
+                point.status = Status.BUSY
                 points[point.x][point.y].status = Status.BUSY
+                #print("busy", point.x, point.y)
             if INNER_RANGE < points_distance(point, chosen_point) <= OUTER_RANGE and not point.status == Status.STATION:
+                point.status = Status.AVAILABLE
                 points[point.x][point.y].status = Status.AVAILABLE
 
 
 points = [[Point(i, j) for i in range(X_SIZE)] for j in range(Y_SIZE)]
 generate_topology()
+print("result: \n")
+for x_row in points:
+    for point in x_row:
+        if point.status == Status.STATION:
+            print("station ", point.x, point.y)
+
+        #if point.status == Status.AVAILABLE:
+        #    print("available ", point.x, point.y)
+
+        #if point.status == Status.FREE:
+        #    print("free ", point.x, point.y)
+
+        #if point.status == Status.BUSY:
+        #    print("busy ", point.x, point.y)
