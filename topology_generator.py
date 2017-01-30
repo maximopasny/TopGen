6f14f7e4917933aa43823fad2e915e8e70e69e1b
maximopasny <maximdudin1@gmail.com>
2017-01-30 21:20:59 +0300

    backup

--- topology_generator.py
+++ topology_generator.py
@@ -2,43 +2,48 @@ from random import choice
 from utils import Point
 from utils import points_distance
 from utils import Status
+from PIL import Image
+import xml.etree.ElementTree as ET
+import time
 
 
-X_SIZE = 40
-Y_SIZE = 40
-INNER_RANGE = 5
-OUTER_RANGE = 20
-NUM_OF_BS = 30
+X_SIZE = 200
+Y_SIZE = 200
+INNER_RANGE = 20
+OUTER_RANGE = 100
+NUM_OF_BS = 40
 
 
-def generate_topology():
-    chosen_point = choice(choice(points))
-    go_iter(chosen_point)
-    available_points = get_available_points()
-    for i in range(0, NUM_OF_BS - 1):
-        iteration_step(available_points)
+def place_stations_opt(grid):
+    chosen_point = choice(choice(grid))
+    chosen_point.status = Status.AVAILABLE
+    for i in range(0, NUM_OF_BS):
+        available_points = get_available_points(grid)
+        chosen_point = choice(available_points)
+        update_points_status_opt(chosen_point, grid)
 
 
-def iteration_step(available_points):
-    chosen_point = choice(available_points)
-    go_iter(chosen_point)
-    available_points = get_available_points()
+def place_stations(grid):
+    chosen_point = choice(choice(grid))
+    chosen_point.status = Status.AVAILABLE
+    for i in range(0, NUM_OF_BS):
+        available_points = get_available_points(grid)
+        chosen_point = choice(available_points)
+        update_points_status(chosen_point, grid)
 
 
-def get_available_points():
+def get_available_points(grid):
     available_points = []
-
-    for x_row in points:
+    for x_row in grid:
         for point in x_row:
             if point.status == Status.AVAILABLE:
                 available_points.append(point)
-
     return available_points
 
 
-def go_iter(chosen_point):
+def update_points_status(chosen_point, grid):
     chosen_point.status = Status.STATION
-    for x_row in points:
+    for x_row in grid:
         for point in x_row:
             if point.status != Status.BUSY and point.status != Status.STATION:
                 distance = points_distance(point, chosen_point)
@@ -48,20 +53,82 @@ def go_iter(chosen_point):
                     point.status = Status.AVAILABLE
 
 
-points = [[Point(i, j) for i in range(X_SIZE)] for j in range(Y_SIZE)]
-generate_topology()
-print("result: \n")
-count = 0
-for x_row in points:
-    for point in x_row:
-        if point.status == Status.STATION:
-            print("station: ", point.x, point.y)
-            count += 1
+def update_points_status_opt(chosen_point, grid):
+    chosen_point.status = Status.STATION
 
-        #if point.status == Status.AVAILABLE:
-        #    print("available: ", point.x, point.y)
+    upper_index = chosen_point.y - INNER_RANGE
+    bottom_index = chosen_point.y + INNER_RANGE
+    left_index = chosen_point.x - INNER_RANGE
+    right_index = chosen_point.x + INNER_RANGE
 
-        #if point.status == Status.BUSY:
-        #   print("busy: ", point.x, point.y)
+    upper_index = (upper_index if upper_index > 0 else 0)
+    bottom_index = (bottom_index if bottom_index < Y_SIZE else Y_SIZE)
+    left_index = (left_index if left_index > 0 else 0)
+    right_index = (right_index if right_index > X_SIZE else X_SIZE)
 
-#print(count)
+    for x_row in grid[upper_index: bottom_index]:
+        for point in x_row[left_index: right_index]:
+            if point.status != Status.BUSY and point.status != Status.STATION:
+                distance = points_distance(point, chosen_point)
+                if distance <= INNER_RANGE:
+                    point.status = Status.BUSY
+                if INNER_RANGE < distance <= OUTER_RANGE:
+                    point.status = Status.AVAILABLE
+
+
+def validator(grid):
+    stations = []
+    for i in range(X_SIZE):
+        for j in range(Y_SIZE):
+            if grid[j][i].status == Status.STATION:
+                stations.append(grid[j][i])
+    distances = []
+    for station in stations:
+        stations.remove(station)
+        for measured_station in stations:
+            distances.append(points_distance(station, measured_station))
+    distances.sort()
+    print(distances)
+    if distances[0] <= INNER_RANGE:
+        print("fail")
+    else:
+        print("success")
+
+
+def create_bitmap(grid):
+    img = Image.new('RGB', (X_SIZE, Y_SIZE), "black")  # create a new black image
+    pixels = img.load()  # create the pixel map
+
+    for i in range(X_SIZE):  # for every pixel:
+        for j in range(Y_SIZE):
+            if grid[j][i].status == Status.STATION:
+                pixels[i, j] = (255, 0, 0)  # red
+            elif grid[j][i].status == Status.BUSY:
+                pixels[i, j] = (0, 0, 255)  # blue
+            elif grid[j][i].status == Status.AVAILABLE:
+                pixels[i, j] = (0, 255, 0)  # green
+            else:
+                pixels[i, j] = (0, 0, 0)    # black
+
+    img.show()
+
+
+def create_xml(grid):
+    root = ET.Element("grid")
+    for i in range(X_SIZE):
+        for j in range(Y_SIZE):
+            if grid[j][i].status == Status.STATION:
+                ET.SubElement(root, "point", {"x_coord": str(i), "y_coord": str(j)})
+    ET.dump(root)
+    tree = ET.ElementTree(root)
+    tree.write("grid.xml")
+
+
+grid = [[Point(i, j) for i in range(X_SIZE)] for j in range(Y_SIZE)]
+start_time = time.time()
+place_stations_opt(grid)
+total_time = time.time() - start_time
+print(total_time)
+create_bitmap(grid)
+create_xml(grid)
+validator(grid)
